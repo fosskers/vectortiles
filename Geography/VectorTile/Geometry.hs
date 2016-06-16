f83db18f3d87a36954cf9f234fa86716f7a7bc4d
Colin Woodbury <colingw@gmail.com>
2016-06-16 14:18:05 -0700

    Can decode polygons

--- Geography/VectorTile/Geometry.hs
+++ Geography/VectorTile/Geometry.hs
@@ -16,10 +16,14 @@
 
 module Geography.VectorTile.Geometry
   ( -- * Geometries
+    -- ** Types
     Geometry(..)
   , Point(..)
   , LineString(..)
   , Polygon(..)
+  -- ** Operations
+  , area
+  , surveyor
   -- * Commands
   , Command(..)
   , commands
@@ -39,6 +43,7 @@ import qualified Data.Vector as V
 import qualified Data.Vector.Unboxed as U
 import           Data.Word
 import           Text.Printf.TH
+import           Data.Foldable (foldlM)
 
 ---
 
@@ -68,6 +73,23 @@ data Polygon = Polygon { points :: U.Vector Point
 newtype Polygon = Polygon { points :: U.Vector Point } deriving (Eq,Show)
 -}
 
+-- | The area of a `Polygon` is the difference between the areas of its
+-- outer ring and inner rings.
+area :: Polygon -> Float
+area p = surveyor (points (p :: Polygon)) + sum (V.map area $ inner p)
+
+-- | The surveyor's formula for calculating the area of a `Polygon`.
+-- If the value reported here is negative, then the `Polygon` should be
+-- considered an Interior Ring.
+--
+-- Assumption: The `U.Vector` given has at least 4 `Point`s.
+surveyor :: U.Vector Point -> Float
+surveyor v = (/ 2) . fromIntegral . U.sum $ U.zipWith3 (\xn yn yp -> xn * (yn - yp)) xs yns yps
+  where v' = U.init v
+        xs = U.map x v'
+        yns = U.map y . U.tail $ U.snoc v' (U.head v')
+        yps = U.map y . U.init $ U.cons (U.last v') v'
+
 -- | Any classical type considered a GIS "geometry". These must be able
 -- to convert between an encodable list of `Command`s.
 class Geometry a where
@@ -91,7 +113,7 @@ instance Geometry Point where
 -- a count greater than 0.
 instance Geometry LineString where
   fromCommands cs = evalState (f cs) (0,0)
-    where f (MoveTo p : LineTo ps : rs) = (fmap . V.cons) <$> ls <*> f rs
+    where f (MoveTo p : LineTo ps : rs) = fmap . V.cons <$> ls <*> f rs
             where ls = LineString <$> U.mapM expand (p <> ps)
           f [] = pure $ Right V.empty
           f _  = pure $ Left "LineString decode: Invalid command sequence given."
@@ -105,8 +127,35 @@ instance Geometry LineString where
             pure [MoveTo $ U.singleton (x h - x curr, y h - y curr), LineTo l]
 
 -- Need a generalized parser for this.
+-- | A valid `R.Feature` of polygons must contain at least one sequence of:
+--
+-- An Exterior Ring, followed by 0 or more Interior Rings.
+--
+-- Any Ring must have a `MoveTo` with a count of 1, a single `LineTo`
+-- with a count of at least 2, and a single `ClosePath` command.
+--
+-- Performs no sanity checks for malformed Interior Rings.
 instance Geometry Polygon where
-  fromCommands = undefined
+  fromCommands cs = do
+    ps <- evalState (f cs) (0,0)
+    let (h,t) = (V.head ps, V.tail ps)
+        (ps',p') = runState (foldlM g V.empty t) h
+    pure $ V.snoc ps' p'  -- Include the last Exterior Ring worked on.
+    where f (MoveTo p : LineTo ps : ClosePath : rs) = do
+            curr <- get
+            let h = U.head p
+                here = (x h + x curr, y h + y curr)
+            po <- flip U.snoc here <$> U.mapM expand (U.cons h ps)
+            fmap (V.cons (Polygon po V.empty)) <$> f rs
+          f [] = pure $ Right V.empty
+          f _  = pure . Left $ [st|Polygon decode: Invalid command sequence given: %s|] (show cs)
+          g acc p | area p > 0 = do  -- New external rings.
+                      curr <- get
+                      put p
+                      pure $ V.snoc acc curr
+                  | otherwise = do  -- Next internal ring.
+                      modify (\s -> s { inner = V.snoc (inner s) p })
+                      pure acc
 
   toCommands = undefined
 
@@ -195,3 +244,11 @@ collapse p = do
   let diff = (x p - x curr, y p - y curr)
   put p
   pure diff
+
+-- Two external rings
+foo :: Either Text (V.Vector Polygon)
+foo = commands [9,4,4,18,6,4,5,4,15,9,4,4,18,6,4,5,4,15] >>= fromCommands @Polygon
+
+-- One external, one internal
+bar :: Either Text (V.Vector Polygon)
+bar = commands [9,4,4,26,6,0,0,6,5,0,15,9,2,3,26,0,2,2,0,0,1,15] >>= fromCommands @Polygon
