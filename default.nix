7cedb9590b11ea88ce6232012ebddf3b8f1d90cf
Colin Woodbury <colingw@gmail.com>
2018-01-13 11:39:39 -0800

    Remove dependency on `protobuf`

--- default.nix
+++ default.nix
@@ -1,24 +1,24 @@
 { mkDerivation, base, bytestring, cereal, containers, criterion
-, deepseq, hex, microlens, microlens-platform, protobuf
-, protocol-buffers, protocol-buffers-descriptor, stdenv, tasty
-, tasty-hunit, text, transformers, vector
+, deepseq, hex, microlens, microlens-platform, protocol-buffers
+, protocol-buffers-descriptor, stdenv, tasty, tasty-hunit, text
+, transformers, vector
 }:
 mkDerivation {
   pname = "vectortiles";
   version = "1.2.0.7";
   src = ./.;
   libraryHaskellDepends = [
-    base bytestring cereal containers deepseq protobuf protocol-buffers
+    base bytestring cereal containers deepseq protocol-buffers
     protocol-buffers-descriptor text transformers vector
   ];
   testHaskellDepends = [
-    base bytestring cereal containers hex protobuf protocol-buffers
+    base bytestring cereal containers hex protocol-buffers
     protocol-buffers-descriptor tasty tasty-hunit text vector
   ];
   benchmarkHaskellDepends = [
     base bytestring cereal containers criterion microlens
-    microlens-platform protobuf protocol-buffers
-    protocol-buffers-descriptor text vector
+    microlens-platform protocol-buffers protocol-buffers-descriptor
+    text vector
   ];
   homepage = "https://github.com/fosskers/vectortiles";
   description = "GIS Vector Tiles, as defined by Mapbox";
